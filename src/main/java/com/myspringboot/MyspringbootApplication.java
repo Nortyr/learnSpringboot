3c12deec613fb7593d0eef2a649cf458d319777a
Nortyr <321785796@qq.com>
2019-01-10 18:28:22 +0800

    2019/1/10 day01 注解学习ing

--- src/main/java/com/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/myspringboot/MyspringbootApplication.java
@@ -2,8 +2,19 @@ package com.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
+/**
+ * @PropertySource(value = {"classpath:xxx.properties"},ignoreResourceNotFound = true)
+ * 扫描这个路径下的自定义properties，ignoreResourceNotFound:找不到就忽略掉
+ *
+ * @ImportResource(value = {"classpath:myxml.xml"})
+ * 扫描这个路径下的xml
+ */
 @SpringBootApplication
+@PropertySource(value = {"classpath:xxx.properties"},ignoreResourceNotFound = true)
+//@ImportResource(value = {"classpath:myxml.xml"})
 public class MyspringbootApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/myspringboot/MyspringbootApplicationTests.java
+++ src/test/java/com/myspringboot/MyspringbootApplicationTests.java
@@ -1,9 +1,12 @@
 package com.myspringboot;
 
 import com.myspringboot.annotation.ComponentDemo;
+import com.myspringboot.annotation.YmlProperties;
+import com.myspringboot.annotation.animal.Animal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,16 +15,40 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MyspringbootApplicationTests {
     @Autowired
     ComponentDemo componentDemo;
+    @Autowired(required = false)
+    @Qualifier("dog")
+    Animal animal;
+//    Animal dog;
+
+    @Autowired
+    YmlProperties ymlProperties1;
+
+    @Autowired
+    YmlProperties ymlProperties;
     @Test
     public void contextLoads() {
     }
 
     @Test
-
     public void componentDemo() {
         System.out.println(componentDemo);
 
     }
 
+    @Test
+
+    public void autoWiredDemo(){
+        System.out.println(animal.getClass().toString());
+       ;
+    }
+
+    @Test
+    public void PropertiesDemo(){
+        System.out.println(ymlProperties.getPort());
+    }
+    @Test
+    public void PropertiesDemo1(){
+        System.out.println(ymlProperties1.getPort());
+    }
 }
 
